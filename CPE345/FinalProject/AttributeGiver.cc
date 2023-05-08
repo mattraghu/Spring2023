ca2e494d1ec8d20c94c39b5560adb57122f8df00
mattraghu <mattraghu@aol.com>
2023-05-08 15:33:42 -0400

    Attribute Giver Update

--- CPE345/FinalProject/AttributeGiver.cc
+++ CPE345/FinalProject/AttributeGiver.cc
@@ -8,16 +8,5 @@ void AttributeGiver::randomPrint()
 void AttributeGiver::handleMessage(cMessage *msg)
 {
     EV << "Handling message `" << msg->getName() << "'\n";
-
-    if (msg->isSelfMessage())
-    {
-        randomPrint();
-    }
-    else
-    {
-        // Schedule Print
-
-        scheduleAt(simTime() + 1, msg);
-        Router::handleMessage(msg);
-    }
+    Router::handleMessage(msg);
 }
