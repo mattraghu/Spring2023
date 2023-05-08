9a77359287c964c71492ac5593ec327ddfb90728
Ryan Potenza <rpotenza@stevens.edu>
2023-05-08 18:26:35 -0400

    Add abandonmentTime in AttributeGiver module

--- CPE345/FinalProject/AttributeGiver.cc
+++ CPE345/FinalProject/AttributeGiver.cc
@@ -4,5 +4,24 @@ Define_Module(AttributeGiver);
 
 void AttributeGiver::handleMessage(cMessage *msg)
 {
-    Router::handleMessage(msg);
+    EV << "Handling message `" << msg->getName() << "'\n";
+
+
+    // Generate a random value with a normal distribution with mean 7 minutes, std of 3 minutes
+       double abandonmentTime;
+    // Ensure the abandonment time isn't negative or 0
+       do {
+           abandonmentTime = normal(7.0*60, 3.0*60);
+
+       } while (abandonmentTime <= 0);
+
+       // Create a parameter object and set its value
+       cPar *abandonmentTimePar = new cPar("abandonmentTime");
+       abandonmentTimePar->setDoubleValue(abandonmentTime);
+
+       // Add the parameter to the message
+       msg->addPar(abandonmentTimePar);
+
+       // Call the parent class's handleMessage() function
+       Router::handleMessage(msg);
 }
