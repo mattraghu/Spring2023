7d4ebaff5f989a693cfff5d608a35a79ea1bf519
mattraghu <mattraghu@aol.com>
2023-05-08 17:07:54 -0400

    Custom Server Init

---
+++ CPE345/FinalProject/CustomServer.h
@@ -0,0 +1,15 @@
+#ifndef __FINALPROJECT_CUSTOMSERVER_H_
+#define __FINALPROJECT_CUSTOMSERVER_H_
+
+#include <omnetpp.h>
+#include <Server.h>
+
+using namespace omnetpp;
+
+class CustomServer : public queueing::Server
+{
+protected:
+  virtual void handleMessage(cMessage *msg) override;
+};
+
+#endif
